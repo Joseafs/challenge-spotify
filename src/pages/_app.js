450f06516a0cfab16e52889d279c5811b272a8e4
Jose.Sabino <joseafs@outlook.com.br>
2020-12-19 23:52:46 -0300

    Config material ui

---
+++ src/pages/_app.js
@@ -0,0 +1,32 @@
+import React, { Fragment, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+
+import theme from '../theme'
+import '../theme/globals.css'
+
+export default function MyApp(props) {
+  const { Component, pageProps } = props
+
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+
+  return (
+    <Fragment>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Fragment>
+  )
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+}
---
+++ src/theme/index.js
@@ -0,0 +1,17 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#999',
+    },
+    secondary: {
+      main: '#333',
+    },
+    background: {
+      default: '#212121',
+    },
+  },
+})
+
+export default theme
